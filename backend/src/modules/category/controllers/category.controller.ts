864ca9711434814f89b462438c0aadccb5ac568a
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-10-10 10:36:48 +0700

    add functional requirement doc

---
+++ backend/src/modules/category/controllers/category.controller.ts
@@ -0,0 +1,7 @@
+import { Response } from "express"
+import { RequestCustom } from "../../../types/express.type"
+
+export const getAllCategory= async(  req: RequestCustom,
+  res: Response): Promise<any>=>{
+    
+  }
--- backend/src/modules/category/models/category.model.ts
+++ backend/src/modules/category/models/category.model.ts
@@ -1,18 +1,13 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface ICategory extends Document {
-  _id: mongoose.Types.ObjectId;
-  userId: mongoose.Types.ObjectId;
   categoryName: string;
   categoryImamge: string;
-  createdAt: Date;
 }
 const categorySchema = new Schema<ICategory>(
   {
-    userId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     categoryName: { type: String, required: true },
     categoryImamge: { type: String },
-    createdAt: { type: Date, default: Date.now },
   },
   {
     timestamps: true,
